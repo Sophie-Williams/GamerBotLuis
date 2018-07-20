57a273061b811e2010c55fd0576b987aa9800350
Amin Chouaibi El Azaar <achouaibi@pasiona.es>
2018-07-20 13:21:27 +0200

    Implemented the Classic and Extended functional game

--- GamerBotLuis/Dialogs/GamerDialog.cs
+++ GamerBotLuis/Dialogs/GamerDialog.cs
@@ -13,7 +13,6 @@ using System.Web;
 namespace GamerBotLuis.Dialogs
 {
     [LuisModel("de5a093d-b90d-46e1-817e-63d9244745d8", "931486fbce2e45ddb5a32c33c8db8341")]
-
     [Serializable]
     public class GamerDialog : LuisDialog<IRockPaperScissorsGame>
     {
@@ -21,35 +20,21 @@ namespace GamerBotLuis.Dialogs
         public const string INTENT_WELCOME = "DarBienvenida";
         public const string INTENT_CHOOSEGAME = "ElegirJuego";
         public const string INTENT_CHOOSEHAND = "ElegirMano";
+        public const string INTENT_PLAYGAME = "Jugar";
+        public const string INTENT_PLAYREMATCH = "ElegirRevancha";
         public const string INTENT_BYE = "DespedirBot";
 
         IRockPaperScissorsGame game;
+        IAwaitable<IMessageActivity> argumentG;
         HandType hand;
+        ExtendedHandType extendedHand;
         HandType winnerHand;
         HandType randomHand;
-
-        public async Task StartAsync(IDialogContext context)
-        {
-            context.Wait(MessageReceived);
-        }
-
-        public async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            
-            if (game.GetType() == typeof(ClassicRockPaperScissorsGame))
-            {
-                randomHand = RandomHand.GetRandomHand();
-                winnerHand = game.Play(hand,randomHand);
-            }
-            else if (game.GetType() == typeof(ExtendedGame))
-            {
-                randomHand = RandomExtendedHand.GetRandomHand();
-                winnerHand = game.Play(hand, randomHand);
-            }
-            await context.PostAsync($"Esta es tu mano: {hand.Name.ToString()} y esta la mía: {randomHand.Name.ToString()} ");
-            await context.PostAsync($"La mano ganadora es: {winnerHand.Name.ToString()} ");
-        }
-
+        Type classic = typeof(ClassicRockPaperScissorsGame);
+        Type extended = typeof(ExtendedGame);
+        EntityRecommendation handEntity;
+        EntityRecommendation extendedHandEntity;
+        bool rematching = false;
 
         [LuisIntent(INTENT_NONE)]
         public async Task None(IDialogContext context, LuisResult result)
@@ -57,7 +42,7 @@ namespace GamerBotLuis.Dialogs
             await context.PostAsync("Lo siento pero no estoy programado para estas opciones. ");
             await Task.Delay(2000);
             await context.PostAsync("En que más te puedo ayudar?");
-            
+
         }
 
         [LuisIntent(INTENT_WELCOME)]
@@ -65,38 +50,37 @@ namespace GamerBotLuis.Dialogs
         {
             if (result.TryFindEntity("Saludo", out var greeting))
             {
-                await context.PostAsync("Estoy programado para jugar al Piedra, Papel, Tijeras." +
-                " Tanto el clasico como el extendido, dime con cúal quieres jugar.");
+                await context.PostAsync("Estoy programado para jugar al Piedra, Papel, Tijeras.");
+                await context.PostAsync("Tanto el clasico como el extendido, dime con cúal quieres jugar.");
             }
         }
 
         [LuisIntent(INTENT_CHOOSEGAME)]
-        public async Task ElejirJuego(IDialogContext context, LuisResult result)
+        public async Task ChoosGame(IDialogContext context, LuisResult result)
         {
             if (result.TryFindEntity("Juego::Clasico", out var classicGameEntity))
             {
                 await context.PostAsync("Has elegido el juego clasico");
                 game = new ClassicRockPaperScissorsGame();
+                await context.PostAsync("Debes elegir un tipo de mano!!");
+                await context.PostAsync("Cuál eliges entre estas: Papel, Piedra o Tijeras ");
             }
-            else if (result.TryFindEntity("Juego::Extendido", out var extGameEntity))
+
+            if (result.TryFindEntity("Juego::Extendido", out var extGameEntity))
             {
                 await context.PostAsync("Has elegido el juego extendido");
                 game = new ExtendedGame();
+                await context.PostAsync("¿Debes elegir un tipo de mano: Papel, Piedra, Tijeras, Lagarto, Spock !!");
+                await Task.Delay(2000);
+                await context.PostAsync("¿Cuál eliges?");
             }
-            await context.PostAsync("Debes elegir un tipo de mano: Papel, Piedra o Tijeras ");
-            await context.PostAsync("Cuál eliges?");
-            
         }
 
         [LuisIntent(INTENT_CHOOSEHAND)]
         public async Task ChooseHandType(IDialogContext context, LuisResult result)
         {
-            Type classic = typeof(ClassicRockPaperScissorsGame);
-            Type extended = typeof(ExtendedGame);
-            EntityRecommendation handEntity;
             if (game.GetType() == classic)
             {
-
                 if (result.TryFindEntity("Mano::Piedra", out handEntity))
                 {
                     await context.PostAsync("Has elegido Piedra");
@@ -111,43 +95,73 @@ namespace GamerBotLuis.Dialogs
                 {
                     await context.PostAsync("Has elegido Tijeras");
                     hand = HandType.Scissors;
-
                 }
-                context.Wait(MessageReceived);
             }
-            else
-            {
-                await context.PostAsync("¿Debes elegir un tipo de mano: Papel, Piedra, Tijeras, Lagarto, Spock !!");
-                await Task.Delay(2000);
-                await context.PostAsync("¿Cuál eliges?");
 
-                if (result.TryFindEntity("Mano::Piedra", out handEntity))
+            if (game.GetType() == extended)
+            {
+                if (result.TryFindEntity("Mano::Piedra", out extendedHandEntity))
                 {
                     await context.PostAsync("Has elegido Piedra");
-                    hand = HandType.Stone;
+                    extendedHand = (ExtendedHandType)HandType.Stone;
                 }
-                else if (result.TryFindEntity("Mano::Papel", out handEntity))
+                else if (result.TryFindEntity("Mano::Papel", out extendedHandEntity))
                 {
                     await context.PostAsync("Has elegido Papel");
-                    hand = HandType.Paper;
+                    extendedHand = (ExtendedHandType)HandType.Paper;
                 }
-                else if (result.TryFindEntity("Mano::Tijeras", out handEntity))
+                else if (result.TryFindEntity("Mano::Tijeras", out extendedHandEntity))
                 {
                     await context.PostAsync("Has elegido Papel");
-                    hand = HandType.Paper;
+                    extendedHand = (ExtendedHandType)HandType.Paper;
                 }
-                else if (result.TryFindEntity("Mano::Lagarto", out handEntity))
+                else if (result.TryFindEntity("Mano::Lagarto", out extendedHandEntity))
                 {
                     await context.PostAsync("Has elegido Lagarto");
-                    hand = ExtendedHandType.Lizard;
+                    extendedHand = ExtendedHandType.Lizard;
                 }
-                else if (result.TryFindEntity("Mano::Spock", out handEntity))
+                else if (result.TryFindEntity("Mano::Spock", out extendedHandEntity))
                 {
                     await context.PostAsync("Has elegido Spock");
-                    hand = ExtendedHandType.Spock;
+                    extendedHand = ExtendedHandType.Spock; //TO DO: Change the language of visual studio for doing all on English! Cause Luis can't understand Spock with Spanish.
                 }
-               
             }
-        }  
+
+            await context.PostAsync("Ya elejiste, cuando quieras empezamos a jugar!");
+        }
+
+        [LuisIntent(INTENT_PLAYGAME)]
+        public async Task PlayGame(IDialogContext context, LuisResult result)
+        {
+            //if (!rematching) TO DO: Implementation of my rematch option;
+            //{
+                if (game.GetType() == typeof(ClassicRockPaperScissorsGame))
+                {
+                    randomHand = RandomHand.GetRandomHand();
+                    winnerHand = game.Play(hand, randomHand);
+                }
+                else if (game.GetType() == typeof(ExtendedGame))
+                {
+                    randomHand = RandomExtendedHand.GetRandomHand();
+                    winnerHand = game.Play(extendedHand, randomHand);
+                }
+                await context.PostAsync($"Esta es tu mano: {hand.Name.ToString()} y esta la mía: {randomHand.Name.ToString()} ");
+                await context.PostAsync($"La mano ganadora es: {winnerHand.Name.ToString()} ");
+            //}
+            //else if (rematching)
+            //{
+            //    await PlayRematch(context, result);
+            //}
+        }
+
+        //[LuisIntent(INTENT_PLAYREMATCH)]
+        //public async Task PlayRematch(IDialogContext context, LuisResult result)
+        //{
+        //    if (result.TryFindEntity("Revancha", out var rematchEntity))
+        //    {
+        //        await ChooseHandType(context, result);
+        //        await PlayGame(context, result);
+        //    }
+        //}
     }
 }
